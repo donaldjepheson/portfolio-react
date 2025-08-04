eaab3ed8d5386cd66ed10e2715d4a2322597500d
Chris-Donald <chrisdonaldwork@gmail.com>
2025-08-03 18:26:05 -0700

    portfolioReact

---
+++ src/components/Contact/data.ts
@@ -0,0 +1,9 @@
+const CONTACTS = {
+	phoneNumber: '+17633289642',
+	email: 'andresrg290507@gmail.com',
+	github: 'https://github.com/donaldjepheson',
+	linkedin: 'https://linkedin.com/in/baatchia-riyadh',
+	twitter: '',
+	whatsapp: '',
+};
+export default CONTACTS;
